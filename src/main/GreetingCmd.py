d3f95cf169746420b18305acd1afcab842935c1b
shubhamshah <shahshubham124@gmail.com>
2020-05-21 01:24:07 -0500

    adding dictonaries

---
+++ src/main/GreetingCmd.py
@@ -0,0 +1,17 @@
+
+# welcome greeting
+
+welcomeGreetingDict = {
+                       'hello': 'hello', 'hi': 'hi', 'how_are_you': 'i am fine we are ready to work', 'i_am_ready': 'ready',
+                       'ram_ram': 'ram ram', 'hello_system': 'hello system', 'are_you_listening': 'yes i am listening','good_morning': 'very good morning',
+                       'lazy_day': 'is today is monday', 'chalo_kam_karte_hain': 'haan chalo', 'today': 'yes today',
+                       }
+
+
+
+afterGreetingDict = {
+                       'by': 'by','bye': 'bye','ok_pack_up': 'ok pack up','band_kare': 'as you wish','enough_for_today': 'yes',
+                       'done': 'i know','i_am_not_able_to_work': 'ok take rest','are_you_heating': 'yes','take_rest': 'you too',
+                       'done': 'done by','ok_by': 'ok by',
+                    }
+
